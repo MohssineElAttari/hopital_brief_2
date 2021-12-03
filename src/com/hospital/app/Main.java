b42a34e14fe923d072915357846edf500a631f91
Mohssine Elattari <elattari.mohssine@gmail.com>
2021-12-03 14:58:20 +0100

    update Main.java

--- src/com/hospital/app/Main.java
+++ src/com/hospital/app/Main.java
@@ -1,5 +1,19 @@
 package com.hospital.app;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		int a, b, c;
+
+		System.out.println("Enter 1st number: ");
+		a = scan.nextInt();
+		System.out.println("Enter 2nd number: ");
+		b = scan.nextInt();
+		c = a + b;
+		System.out.println("The sum is " + c);
+		scan.close();
+	}
 }
