a09b5504aaa895efdef82a60298b50e59531e02a
Mohssine Elattari <elattari.mohssine@gmail.com>
2021-12-03 15:01:03 +0100

    update OperationInterface.java

--- src/com/hospital/interfaces/OperationInterface.java
+++ src/com/hospital/interfaces/OperationInterface.java
@@ -1,14 +1,26 @@
 package com.hospital.interfaces;
 
+import com.hospital.models.Hospital;
 import com.hospital.models.Operation;
 import java.util.Optional;
 import java.util.List;
 
-
 public interface OperationInterface {
 
-	public Optional<Operation> getOperationByRef(int ref);
-	
-	public void addOperation(List<Operation> operations);
-	
+	public Optional<Operation> getOperationByRef(int ref, Hospital hospital);
+
+	public void addOperation(List<Operation> operations, Operation operation);
+
+	public boolean verifyInsurance();
+
+	public double calculateRsefund();
+
+	public double priceTopay();
+
+	public void checkPayementStatus();
+
+	void exceedPayement();
+
+	public void addToHospitalOparationList();
+
 }
